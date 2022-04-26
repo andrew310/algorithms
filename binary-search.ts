86d737bb1a88a9e73e03e24665d4ff01146a2d8a
Andrew Brown <andrewbrown600@gmail.com>
2022-04-26 12:07:52 -0700

    init

---
+++ binary-search.ts
@@ -0,0 +1,19 @@
+
+function search(nums: number[], target: number): number {
+    let startpoint = 0,
+      endpoint = nums.length - 1,
+      midpoint = Math.floor((endpoint - startpoint) / 2)
+
+    while (startpoint < endpoint) {
+        midpoint = startpoint + Math.floor((endpoint - startpoint) / 2)
+        if (nums[midpoint] < target) {
+            startpoint = midpoint + 1
+        } else if (nums[midpoint] > target) {
+            endpoint = midpoint - 1
+        }
+        if(nums[midpoint] === target) return midpoint
+    }
+
+    return -1
+}
+
